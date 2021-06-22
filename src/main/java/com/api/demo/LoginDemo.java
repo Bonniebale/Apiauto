81e1814d8674986f15c5cdd952ee6dd4d06302f5
duruo <luguiwan@163.com>
2021-06-22 21:41:12 +0800

    解决登录失败的问题，但是没有办法获取cookie并传递

--- src/main/java/com/api/demo/LoginDemo.java
+++ src/main/java/com/api/demo/LoginDemo.java
@@ -9,7 +9,9 @@ import com.api.model.User;
 import com.api.restclient.RestClient;
 import com.api.utils.ConfigFile;
 import com.api.utils.TestUtil;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -17,6 +19,8 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class LoginDemo extends BaseTest {
     public String url;
@@ -37,44 +41,48 @@ public class LoginDemo extends BaseTest {
     public void login() throws IOException {
         Company company = new Company();
         restClient = new RestClient();
+        //添加请求参数
+        List<NameValuePair> params = new LinkedList<>();
+        BasicNameValuePair param1 = new BasicNameValuePair("companyName","Nono测试");
+        BasicNameValuePair param2 = new BasicNameValuePair("userName","admin");
+        BasicNameValuePair param3 = new BasicNameValuePair("password","55A86C51427E48F486272A465CE15D73");
+        params.add(param1);
+        params.add(param2);
+        params.add(param3);
+        System.out.println("params:"+params);
+
         //准备请求头信息
         HashMap<String,String> headerMap = new HashMap<String,String>();
         headerMap.put("Content-Type","application/json");
 
-        JSONObject param = new JSONObject();
-        param.put("测试最棒",company.getCompanyName());
-        param.put("admin",company.getUserName());
-        param.put("cjzg123456",company.getPassword());
-        System.out.println("param:"+param);
 
 
+//         第二种
+//        JSONObject param = new JSONObject();
+//        param.put("测试最棒",company.getCompanyName());
+//        param.put("admin",company.getUserName());
+//        param.put("cjzg123456",company.getPassword());
+//        System.out.println("param:"+param);
+
+//第一种
         //对象转成JSON字符串
 //        Company company = new Company("测试最棒","admin","cjzg123456");
 //        String companyJSONString =JSON.toJSONString(company);
 //        System.out.println("companyJSONString:"+companyJSONString);
 
-        closeableHttpResponse = restClient.post(url,headerMap,param);
+        closeableHttpResponse = restClient.post(url,headerMap,params);
 //        closeableHttpResponse = restClient.post(url,companyJSONString,headerMap);
         //验证状态码是不是200
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
-        Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_200,"status code is not 201.");
+        Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_200,"status code is not 200.");
         System.out.println("statusCode:"+statusCode);
+
+        String status ;
 
         //断言响应json中name和job是不是期待结果
         String responseString = EntityUtils.toString(closeableHttpResponse.getEntity());
         JSONObject responseJson = JSON.parseObject(responseString);//是将str转化为相应的JSONObject对象
         System.out.println(responseString);
-//
-//        String userName = TestUtil.getValueByJPath(responseJson,"userName");
-//        String companyName =TestUtil.getValueByJPath(responseJson,"companyName");
-//        String password = TestUtil.getValueByJPath(responseJson,"password");
-//
-//        Assert.assertEquals(userName,"","");
-//        Assert.assertEquals(companyName,"","");
-//        Assert.assertEquals(userName,"","");
-
-
-
 
 
         restClient.get(url);
---
+++ src/main/java/com/api/demo/TradeOrder.java
@@ -0,0 +1,2 @@
+package com.api.demo;public class TradeOrder {
+}
